4223ba5de424eb9f6842701d7f529d9bbb76104d
naveen21kumar95 <naveen21kumar95@gmail.com>
2026-02-20 16:54:23 -0500

    Variables with PIN updated

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo "Enter your PIN number:"
-read PIN
+echo "Enter your Debit PIN number:"
+read DebitPIN
 
-echo "You entered: $PIN"
+echo "You entered: $DebitPIN"
+
+echo "Enter your Credit PIN number:"
+read -s CreditPIN
+
+echo "You entered: $CreditPIN"
