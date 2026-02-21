73454902f56011fe763089154b9249241335f83b
naveen21kumar95 <naveen21kumar95@gmail.com>
2026-02-20 23:02:39 -0500

    Array Datatype updated

--- 07-array-datatype.sh
+++ 07-array-datatype.sh
@@ -5,3 +5,5 @@ MOVIES=("Movie1", "Movie2", "Movie3")
 echo "The first movie in the array is: ${MOVIES[0]}"
 echo "The second movie in the array is: ${MOVIES[1]}"
 echo "The third movie in the array is: ${MOVIES[2]}"
+echo "All movies in the array: ${MOVIES[@]}"
+echo "Number of movies in the array: ${#MOVIES[@]}"
