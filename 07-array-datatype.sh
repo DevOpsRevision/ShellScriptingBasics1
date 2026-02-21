06f91de61c3a6b3896c7725ec11f9193828ba741
naveen21kumar95 <naveen21kumar95@gmail.com>
2026-02-20 22:59:15 -0500

    Array Datatype

---
+++ 07-array-datatype.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MOVIES=("Movie1", "Movie2", "Movie3")
+
+echo "The first movie in the array is: ${MOVIES[0]}"
+echo "The second movie in the array is: ${MOVIES[1]}"
+echo "The third movie in the array is: ${MOVIES[2]}"
