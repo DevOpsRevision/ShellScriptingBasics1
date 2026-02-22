4e16918ba4b4e9b030117676e5738f460e76eae6
naveen21kumar95 <naveen21kumar95@gmail.com>
2026-02-21 20:35:17 -0500

    Special Variables

---
+++ 08-special-vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "The name of the script is: $0"
+echo "The first argument is: $1"
+echo "The second argument is: $2"
+echo "The total number of arguments is: $#"
+echo "All arguments passed to the script: $@"
+echo "The process ID of the current script is: $$"
+echo "The process ID of the last background command is $!"
+echo "The exit status of the last command is: $?"
