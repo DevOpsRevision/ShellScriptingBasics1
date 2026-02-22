7de754c933b9f70cbf97c62797dc14b39d10b418
naveen21kumar95 <naveen21kumar95@gmail.com>
2026-02-21 21:07:36 -0500

    Special Variables

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -6,5 +6,6 @@ echo "The second argument is: $2"
 echo "The total number of arguments is: $#"
 echo "All arguments passed to the script: $@"
 echo "The process ID of the current script is: $$"
+sleep 5 &
 echo "The process ID of the last background command is $!"
 echo "The exit status of the last command is: $?"
