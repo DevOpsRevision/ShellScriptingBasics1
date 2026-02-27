620ab65f551c365a05600affcb266de06ddf1bd1
naveen21kumar95 <naveen21kumar95@gmail.com>
2026-02-26 21:47:17 -0500

    logs usage

--- 14-logs.sh
+++ 14-logs.sh
@@ -2,6 +2,7 @@
 # This script demonstrates the use of functions in bash scripting
 
 USERID=$(id -u)
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -11,6 +12,14 @@ LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(basename "$0" | cut -d "." -f 1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
+
+# Root user validation and logging the output to a log file
+if [ $USERID -ne 0 ]; then
+  echo -e "$R ERROR :: Please run as root user. $N"
+  exit 1 # Exit with a non-zero status to indicate an error
+fi
+
+mkdir -p "$LOGS_FOLDER"
 
 echo "Script execution started at: $(date)" &>>$LOG_FILE
 
@@ -22,7 +31,14 @@ else
   echo "You are root user. Proceeding with the installation..." &>>$LOG_FILE
 fi
 
-mkdir -p "$LOGS_FOLDER"
+VALIDATE(){
+  if [ $1 -ne 0 ]; then
+    echo -e "$2 installation is .... $R FAILURE $N" &>>$LOG_FILE
+    exit 1 # Exit with a non-zero status to indicate an error
+  else
+    echo -e "$2 installation is .... $G SUCCESSFUL. $N" &>>$LOG_FILE
+  fi
+}
 
 dnf list installed mysql
 
@@ -34,13 +50,3 @@ else
    echo -e "Mysql is already installed. $Y No action needed. $N" &>>$LOG_FILE
    exit 0 # Exit with a zero status to indicate success
 fi
-
-
-VALIDATE(){
-  if [ $1 -ne 0 ]; then
-    echo -e "$2 installation is .... $R FAILURE $N" &>>$LOG_FILE
-    exit 1 # Exit with a non-zero status to indicate an error
-  else
-    echo -e "$2 installation is .... $G SUCCESSFUL. $N" &>>$LOG_FILE
-  fi
-}
