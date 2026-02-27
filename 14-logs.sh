1bc056fd0292eaa2f1e980cee29b47b9b2e4a7f5
naveen21kumar95 <naveen21kumar95@gmail.com>
2026-02-26 21:53:30 -0500

    logs usage

--- 14-logs.sh
+++ 14-logs.sh
@@ -21,32 +21,33 @@ fi
 
 mkdir -p "$LOGS_FOLDER"
 
-echo "Script execution started at: $(date)" &>>$LOG_FILE
+# echo "Script execution started at: $(date)" | tee -a $LOG_FILE
+echo "Script execution started at: $(date)" | tee -a $LOG_FILE
 
 
 if [ $USERID -ne 0 ]; then
-  echo -e "$R ERROR :: You are NOT root user. Please run this script with root priviliges. $N" &>>$LOG_FILE
+  echo -e "$R ERROR :: You are NOT root user. Please run this script with root priviliges. $N" | tee -a $LOG_FILE
   exit 1 # Exit with a non-zero status to indicate an error
 else
-  echo "You are root user. Proceeding with the installation..." &>>$LOG_FILE
+  echo "You are root user. Proceeding with the installation..." | tee -a $LOG_FILE
 fi
 
 VALIDATE(){
   if [ $1 -ne 0 ]; then
-    echo -e "$2 installation is .... $R FAILURE $N" &>>$LOG_FILE
+    echo -e "$2 installation is .... $R FAILURE $N" | tee -a $LOG_FILE
     exit 1 # Exit with a non-zero status to indicate an error
   else
-    echo -e "$2 installation is .... $G SUCCESSFUL. $N" &>>$LOG_FILE
+    echo -e "$2 installation is .... $G SUCCESSFUL. $N" | tee -a $LOG_FILE
   fi
 }
 
 dnf list installed mysql
 
 if [ $? -ne 0 ]; then
-  echo "MySQL is not installed. Proceeding with the installation..." &>>$LOG_FILE
+  echo "MySQL is not installed. Proceeding with the installation..." | tee -a $LOG_FILE
   dnf install mysql -y
   VALIDATE $? "MySQL"
 else
-   echo -e "Mysql is already installed. $Y No action needed. $N" &>>$LOG_FILE
+   echo -e "Mysql is already installed. $Y No action needed. $N" | tee -a $LOG_FILE
    exit 0 # Exit with a zero status to indicate success
 fi
